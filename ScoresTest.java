a937f4709244abcaf4f09b26273e6245b6ee09e6
linblum <BlumMK23@gcc.edu>
2024-10-22 16:40:11 -0400

    comment

--- ScoresTest.java
+++ ScoresTest.java
@@ -1,4 +1,6 @@
 public class ScoresTest {
     //note to self: remember to pull first and commit and push when done
+    //Lin: tests for Scores() constructor
+    //Adam: tests for getMax() method
 
 }
